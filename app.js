a1ce9db9a7429a4f75cf64fe65e3cf6ab0deff6a
Kira Cheng <kira@uber.com>
2017-02-15 22:39:35 +0800

    搭建项目的基本结构

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
-/**
- * Created by kira on 2/15/17.
- */
+var express = require('express');
+var path = require('path');
+var port = process.env.PORT || 3000;
+var app = express();
+
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+app.listen(port);
+
+console.log(`Movie project have been start at port ${port}`);
+
+app.get('/', (req, res) => {
+  //res.sendFile(path.join(__dirname, './index.html'));
+  res.render('index', {
+    title: 'Movie 首页'
+  });
+});
+app.get('/movie/detail/:id', (req, res) => {
+  res.render('detail', {
+    title: 'Movie 详情页'
+  });
+});
+app.get('/admin/movie/list', (req, res) => {
+  res.render('list', {
+    title: 'Movie 列表页'
+  });
+});
+app.get('/admin/movie/new', (req, res) => {
+  res.render('admin', {
+    title: 'Movie 添加电影页'
+  });
+});
